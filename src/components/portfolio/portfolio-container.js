65f6a02128652f27fafd601b94f3174a505e7e12
jasonfletcher <jasonfletchersmail@gmail.com>
2020-06-15 17:04:27 -0600

    storing

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -2,12 +2,26 @@ import React, {Component} from 'react';
 import PortfolioItem from './portfolio-item.js';
 
 export default class PortfolioContainer extends Component {
+   constructor() {
+      super();
+
+      console.log("Portfolio container has rendered");
+   }
+   
+   portfolioItems() {
+      const data = ["Intel", "AtTask", "Solutionreach", "O.C. Tanner"];
+
+      return data.map(item => {
+         return <PortfolioItem />;
+      })
+   }
+
    render() {
       return (
          <div>
             <h2>Portfolio Items Go Here...</h2>
 
-            <PortfolioItem />
+            { this.portfolioItems() }
          </div>
       )
    }
